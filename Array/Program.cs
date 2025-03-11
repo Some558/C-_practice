d58ee5da3b295f091ea8da56588c7ece042ed46e
Some558 <1641043@gmail.com>
2025-03-11 14:00:09 +0900

    Array

---
+++ Array/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> numbers = new List<int>();
+            numbers.Add(10);
+            numbers.Add(25);
+            numbers.Add(37);
+            numbers.Add(42);
+            numbers.Add(55);
+            numbers.Add(68);
+            numbers.Add(79);
+            numbers.Add(81);
+            numbers.Add(96);
+            numbers.Add(100);
+
+            int totalNumber = 0;
+            double oddNumber = 0;
+            int count = 0;
+
+            foreach(int number in numbers)
+            {
+                if(number % 2 == 0)
+                {
+                    totalNumber += number;
+                }
+                else
+                {
+                    oddNumber += number;
+                    count ++;
+                }
+            }
+            Console.WriteLine($"偶数の合計:{totalNumber}");
+            Console.WriteLine($"奇数の平均{oddNumber / count}");
+        }
+    }
+}

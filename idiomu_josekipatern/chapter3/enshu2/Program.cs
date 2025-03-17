11eacff1e33a24571e99783df58b43e17091a972
Some558 <1641043@gmail.com>
2025-03-18 06:34:18 +0900

    enshu2

---
+++ idiomu_josekipatern/chapter3/enshu2/Program.cs
@@ -0,0 +1,17 @@
+﻿var cities = new List<string>
+{
+    "Tokyo", "New Delhi","Bangkok", "London", "Paris", "Berlin",
+     "Canberra", "Hong Kong",
+};
+
+Console.WriteLine("都市名を入力。空行で終了");
+do
+{
+    var line = Console.ReadLine();
+    if (string.IsNullOrEmpty(line))
+        break;
+    var index = cities.FindIndex(s => s == line);
+    Console.WriteLine(index);
+} while (true);
+
+// 明日はこの答えがなぜ-1を示すのか調べるところから

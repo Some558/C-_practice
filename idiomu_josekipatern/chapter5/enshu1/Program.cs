49f42072544bd43015f59330a65e1ef52bfee7ef
Some558 <1641043@gmail.com>
2025-03-26 13:33:58 +0900

    enshu5終了

--- idiomu_josekipatern/chapter5/enshu1/Program.cs
+++ idiomu_josekipatern/chapter5/enshu1/Program.cs
@@ -1,4 +1,4 @@
-﻿public class YearMonth
+﻿public record YearMonth
 {
     public int Year { get; }
     public int Month { get; }
@@ -32,22 +32,56 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        var yearmonth = new YearMonth(2010, 11);
-        Console.WriteLine(yearmonth.ToString()); // 出力: 2010年11月
-
-        var nextMonth = yearmonth.AddOneMonth();
-        Console.WriteLine(nextMonth.ToString()); // 出力: 2010年12月
-
-        Console.WriteLine($"Is 21st Century: {yearmonth.Is21Century}"); // 出力: Is 21st Century: True
-
         var yearmonth2 = new List<YearMonth>
         {
-            new yearmonth2(1234,10),
-            new yearmonth2(1235,6),
-            new yearmonth2(1236,7),
-            new yearmonth2(1237,8),
-            new yearmonth2(1238,9)
+            new YearMonth(1234,10),
+            new YearMonth(1235,6),
+            new YearMonth(1834,7),
+            new YearMonth(1237,8),
+            new YearMonth(1238,9)
         };
-        yearmonth2.ForEach(n => Console.WriteLine($"[{n}]"));
+
+        var first21Century = FindFirst21stCentury(yearmonth2);
+
+        if (first21Century != null)
+        {
+            Console.WriteLine(first21Century.Year);
+        }
+        else
+        {
+            Console.WriteLine("21世紀のデータはありません");
+        }
+
+        var nextMonths = yearmonth2.Select(ym => ym.AddOneMonth()).ToList();
+
+        foreach (var nextMonth in nextMonths)
+        {
+            Console.WriteLine(nextMonth);
+        }
     }
+
+    public static YearMonth FindFirst21stCentury(List<YearMonth> yearmonths)
+    {
+        foreach (var _yearmonth in yearmonths)
+        {
+            if (_yearmonth.Is21Century)
+            {
+                return _yearmonth;
+            }
+        }
+        return null;
+    }
+
+    public static List<YearMonth> AddYearMonth(List<YearMonth> yearMonths)
+    {
+        var result = new List<YearMonth>();
+
+        foreach (var yearMonth in yearMonths)
+        {
+            var nextMonth = yearMonth.AddOneMonth();
+            result.Add(nextMonth);
+        }
+        return result;
+    }
+
 }

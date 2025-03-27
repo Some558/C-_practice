5af72bd6cbc5e6421767bc701424b2be28db469c
Some558 <1641043@gmail.com>
2025-03-28 06:45:42 +0900

    enzanshi

---
+++ ConsoleApp/shisokuenzan/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    static void Main(params string[] args)
+    {
+        // コンソールに演算子を入力。
+        Console.WriteLine("演算子を一つ入力してください。");
+        var enzanshi = Console.ReadLine();
+        Console.WriteLine("数値をスペース空けて入力してください。");
+        var baseValue = Console.ReadLine(int)();
+        var lastValue = 0;
+        var middleValue = 0;
+        if (enzanshi == +)
+        {
+            foreach (var value in baseValue)
+            {
+                lastValue += value;
+            }
+        }
+        else if (enzanshi == *)
+        {
+            for (i = 1; i <= baseValue.length;)
+        }
+
+
+
+
+        // 数字を複数入力し、その数値に対して先ほどの演算子を適用
+        // 適用結果をコンソールに表示する
+    }
+}

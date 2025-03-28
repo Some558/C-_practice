a15eee864382e023ffe288497ab068934c966b59
Some558 <1641043@gmail.com>
2025-03-29 06:54:41 +0900

    デバッグ難航中

--- ConsoleApp/shisokuenzan/Program.cs
+++ ConsoleApp/shisokuenzan/Program.cs
@@ -6,23 +6,41 @@
         Console.WriteLine("演算子を一つ入力してください。");
         var enzanshi = Console.ReadLine();
         Console.WriteLine("数値をスペース空けて入力してください。");
-        var baseValue = Console.ReadLine(int)();
+        var baseValue = Console.ReadLine();
         var lastValue = 0;
-        var middleValue = 0;
-        if (enzanshi == +)
+        var middleValue = 1;
+        if (enzanshi == "+")
         {
             foreach (var value in baseValue)
             {
                 lastValue += value;
             }
+            Console.WriteLine($"演算結果は、{lastValue}です");
         }
-        else if (enzanshi == *)
+        if (enzanshi == "-")
         {
-            for (i = 1; i <= baseValue.length;)
+            foreach (var value in baseValue)
+            {
+                lastValue -= value;
+            }
+            Console.WriteLine($"演算結果は、{lastValue}です");
         }
-
-
-
+        if (enzanshi == "*")
+        {
+            foreach (var value in baseValue)
+            {
+                middleValue *= value;
+            }
+            Console.WriteLine($"演算結果は、{middleValue}です");
+        }
+        if (enzanshi == "/")
+        {
+            foreach (var value in baseValue)
+            {
+                middleValue /= value;
+            }
+            Console.WriteLine($"演算結果は、{middleValue}です");
+        }
 
         // 数字を複数入力し、その数値に対して先ほどの演算子を適用
         // 適用結果をコンソールに表示する
